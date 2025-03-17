cf9eb4d6c04c9fc24c0ae5bc283c3fb5443ec63c
poojan-solanki <blutspoojansolanki@gmail.com>
2025-03-17 16:12:36 +0530

    change rp handler

--- rp_handler.py
+++ rp_handler.py
@@ -5,7 +5,6 @@ import runpod
 from io import BytesIO
 import base64
 from huggingface_hub import login
-import time
 
 
 def handler(event):
@@ -20,23 +19,15 @@ def handler(event):
     #Make string to pillow image
     # b64decode will make it in bytes b'jdhkfghfh' format
     # BytesIO will
-    image = Image.open(BytesIO(base64.b64decode(image))) 
-    # image.show()
-    # print("image read success")
-
-    time.sleep(30)
-    print("reading model id")
+    image = Image.open(BytesIO(base64.b64decode(image)))
 
     # Placeholder for a task; replace with image or text generation logic as needed
     model_id = "meta-llama/Llama-3.2-11B-Vision-Instruct"
-    print("model id read success")
-    print ("loading model")
     model = MllamaForConditionalGeneration.from_pretrained(
         model_id,
         torch_dtype=torch.bfloat16,
         device_map="auto",
     )
-    print("model load complet loading processor")
     processor = AutoProcessor.from_pretrained(model_id)
 
     messages = [
@@ -45,18 +36,20 @@ def handler(event):
             {"type": "text", "text": "Analyze the given image and provide the following details: 1. Count of people present in the image. 2. Identify activities happening in the image (e.g., criminal avtivity, detect weapons, working on a computer, having a conversation, idle, etc.).\n 3. Determine whether individuals are is alone, in groups, or not engaged in work, or are walking. Identify there pose\n4. **Important**: Provide detailed observations about the environment, and identify interaction between various people and objects.\n 5. Highlight any unusual or suspicious behavior (if any).\n\nFormat the response as a structured report for easy understanding."}
         ]}
     ]
-    print("message read")
     input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
-    print ("processing image")
     inputs = processor(
         image,
         input_text,
         add_special_tokens=False,
         return_tensors="pt"
     ).to(model.device)
-    print("returning output")
-    output = model.generate(**inputs, max_new_tokens=30)
-    return processor.decode(output[0])
+    output = model.generate(**inputs, max_new_tokens=512)
+    full_output = processor.decode(output[0], skip_special_tokens=True)
+
+    input_length = len(input_text)
+    generated_text = full_output[input_length:].strip()
+    return generated_text 
+
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler})
