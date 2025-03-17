0d6b37fb50db543163da8d0b4b40f57f2cf08d29
poojan-solanki <blutspoojansolanki@gmail.com>
2025-03-17 15:27:28 +0530

    edit rp handler

--- rp_handler.py
+++ rp_handler.py
@@ -30,14 +30,13 @@ def handler(event):
     # Placeholder for a task; replace with image or text generation logic as needed
     model_id = "meta-llama/Llama-3.2-11B-Vision-Instruct"
     print("model id read success")
-    time.sleep(10)
     print ("loading model")
-    time.sleep(20)
     model = MllamaForConditionalGeneration.from_pretrained(
         model_id,
         torch_dtype=torch.bfloat16,
         device_map="auto",
     )
+    print("model load complet loading processor")
     processor = AutoProcessor.from_pretrained(model_id)
 
     messages = [
@@ -46,16 +45,18 @@ def handler(event):
             {"type": "text", "text": "Analyze the given image and provide the following details: 1. Count of people present in the image. 2. Identify activities happening in the image (e.g., criminal avtivity, detect weapons, working on a computer, having a conversation, idle, etc.).\n 3. Determine whether individuals are is alone, in groups, or not engaged in work, or are walking. Identify there pose\n4. **Important**: Provide detailed observations about the environment, and identify interaction between various people and objects.\n 5. Highlight any unusual or suspicious behavior (if any).\n\nFormat the response as a structured report for easy understanding."}
         ]}
     ]
+    print("message read")
     input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
+    print ("processing image")
     inputs = processor(
         image,
         input_text,
         add_special_tokens=False,
         return_tensors="pt"
     ).to(model.device)
-
+    print("returning output")
     output = model.generate(**inputs, max_new_tokens=30)
-    print(processor.decode(output[0]))
+    return processor.decode(output[0])
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler})
